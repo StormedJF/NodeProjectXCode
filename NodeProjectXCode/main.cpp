28935263a9fdf99bec739f731cc18e53514401ac
StormedJF <Storm@joshs-mbp.lan>
2016-05-11 15:11:02 -0600

    Initial Commit

---
+++ NodeProjectXCode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NodeProjectXCode
+//
+//  Created by Josh on 5/11/16.
+//  Copyright © 2016 Jforou. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
